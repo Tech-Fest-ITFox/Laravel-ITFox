5cdee6546a5e6592d23a1d7de7d16159c735765e
Nikolay <nikolaykolibarov@gmail.com>
2016-05-26 14:28:00 +0300

    Trying to add .env.example file

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer>
-    <a href="/">ITFox</a> 2015 <br />
+    <a href="/">ITFox</a> 2016 <br />
     Всички права запазени  <br />
     Християн Япраков • Николай Колибаров • Даниел Лудиянов<br />
     <a href="http://www.weband.bg" target="_blank">www.weband.bg</a>
